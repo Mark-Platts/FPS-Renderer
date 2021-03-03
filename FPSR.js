be9662654aeec5b111ef5ab7b1b3cf41cc564a87
Mark-Platts <markplattstutoring@outlook.com>
2021-03-03 00:24:10 +0000

    add

--- FPSR.js
+++ FPSR.js
@@ -2,14 +2,17 @@
 const pi = Math.PI;
 const rotAngle = pi/24;
 let lastMousePos = [0,0];
-const worldToCanvasScale = 1;
-const moveSpeed = 1;
+const worldToCanvasScale = 1000;
+const moveSpeed = 10;
 const rotSpeed = 1;
+const mouseRotSpeed = 0.001;
+let mouseFollow = true;
+
 
 
 //camera
 let camera = {
-    position: [0,150,0],
+    position: [0,-150,-2000],
     xunit: [1,0,0],
     yunit: [0,1,0],
     zunit: [0,0,1],
@@ -38,6 +41,11 @@ function rightwardCamMov(camera, magnitude) {
     camera.position = vecAdd(camera.position, vecScale(magnitude, camera.xunit));
 }
 
+//Takes a world coordinate and returns it from the camera pov
+function coordToCam(camera, coord) {
+    return vecDiff(coord, camera.position);
+}
+
 function showCamUnits(camera) {
     console.log(camera.xunit);
     console.log(vecMag(camera.xunit));
@@ -50,13 +58,13 @@ function showCamUnits(camera) {
 
 //world
 const object1 = {
-    coords = [[100,0,0],[100,0,100],[200,0,100],[200,0,0],[100,-100,0],[100,-100,100],[200,-100,100],[200,-100,0]],
-    lines = [[0,1],[1,2],[2,3],[3,0],[4,5],[5,6],[6,7],[7,4],[0,4],[1,5],[2,6],[3,7]]
+    coords: [[100,0,0],[100,0,100],[200,0,100],[200,0,0],[100,100,0],[100,100,100],[200,100,100],[200,100,0]],
+    lines: [[0,1],[1,2],[2,3],[3,0],[4,5],[5,6],[6,7],[7,4],[0,4],[1,5],[2,6],[3,7]]
 }
 
 const object2 = {
-    coords = [[100,0,200],[100,0,300],[200,0,300],[200,0,200],[100,-100,200],[100,-100,300],[200,-100,300],[200,-100,200]],
-    lines = [[0,1],[1,2],[2,3],[3,0],[4,5],[5,6],[6,7],[7,4],[0,4],[1,5],[2,6],[3,7]]
+    coords: [[100,0,200],[100,0,300],[200,0,300],[200,0,200],[100,100,200],[100,100,300],[200,100,300],[200,100,200]],
+    lines: [[0,1],[1,2],[2,3],[3,0],[4,5],[5,6],[6,7],[7,4],[0,4],[1,5],[2,6],[3,7]]
 }
 
 const world = [object1, object2];
@@ -180,129 +188,47 @@ window.addEventListener('resize', windowResize);
 
 
 
-//checks for perspective and returns new scaled coordinates
-//canDist is the distance from (0,0,0) to the center of the canvas
+//Takes a camera and coordinate, returns where coordinate appears on the canvas according to perspective
+//worldToCanvasScale makes things bigger or smaller on the canvas if necessary
 //eyeDist is distance from the center of the canvas to the eye
-//the number*w exaggerates the 4D perspective
-function setPersp(coords, canDist, eyeDist) {
+function setPersp(camera, coords) {
     let hold = [];
-    if (perspectiveEnabled == false) {
-        for (let i = 0; i < coords.length; i++){
-            hold.push(coords[i]);
-        }
-    }
-    else if (perspectiveEnabled == true) {
-        for (let i = 0; i < coords.length; i++){
-            let rastCoord = [];
-            let x = coords[i][0];
-            let y = coords[i][1];
-            let z = coords[i][2];
-            let tz = (canDist - z)/(canDist + eyeDist - z);
-            let tw = 0;
-            if (coords[i].length == 4 && wPerspExagOn == true) {
-                w = coords[i][3]
-                tw = (canDist - wPerspExag*w)/(canDist + eyeDist - wPerspExag*w);
-            } else if (coords[i].length == 4 && wPerspExagOn == false) {
-                w = coords[i][3]
-                tw = (canDist - w)/(canDist + eyeDist - w);
-            }
-            rastCoord.push(x*(1-tz-tw));
-            rastCoord.push(y*(1-tz-tw));
-            hold.push(rastCoord);
-        }
+    const eyeDist = camera.eyeDist;
+    for (let i = 0; i < coords.length; i++){
+        const camCoord = coordToCam(camera, coords[i]);
+        let rastCoord = [];
+        let x = vecDot(camCoord, camera.xunit);
+        let y = vecDot(camCoord, camera.yunit);
+        let z = vecDot(camCoord, camera.zunit);
+        //console.log(z)
+        rastCoord.push(x*eyeDist/z);
+        rastCoord.push(y*eyeDist/z);
+        hold.push(vecScale(worldToCanvasScale,rastCoord));
     }
     return hold;
 }
 
 
 //Takes the coord and line data and renders them to the canvas
-function render(coords){
+function render(world){
     const canCenX = document.getElementById("mainCanvas").width/2;
     const canCenY = document.getElementById("mainCanvas").height/2;
-    //resizes coords so that they will fit the canvas nicely
-    let scaledCoords;
-    if (coords[0].length == 3) {
-        scaledCoords = scaleCoords(coords, 3); //was 4
-    }
-    else if (coords[0].length == 4) {
-        scaledCoords = scaleCoords(coords, 2); //was 1.5
-    }
-    if (perspectiveEnabled == false) {
-        scaledCoords = scaleCoords(coords, 0.95);
-    }
-    const bm = biggestMag(scaledCoords);
-    const eyeDist = 1.5*bm;
-    const canDist = 4*bm;
-    let finCoords = setPersp(scaledCoords, canDist, eyeDist); //returns coords with possible perspective altering
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.strokeStyle = "#FFFFFF";
-    for (let i = 0; i < lines.length; i++){
-        ctx.beginPath();
-        ctx.moveTo(canCenX + finCoords[lines[i][0]][0], canCenY + finCoords[lines[i][0]][1]);
-        ctx.lineTo(canCenX + finCoords[lines[i][1]][0], canCenY + finCoords[lines[i][1]][1]);
-        ctx.stroke();
-    }
-}
-
-//Takes the coord and line data and renders them to the canvas
-function trender(coords){
-    const canCenX = document.getElementById("mainCanvas").width/2;
-    const canCenY = document.getElementById("mainCanvas").height/2;
-    const bm = biggestMag(coords);
-    const eyeDist = bm;
-    const canDist = 4*bm;
-    let perspCoords = setPersp(coords, canDist, eyeDist); //returns coords with possible perspective altering
-    let finCoords = scaleCoords(perspCoords, 0.95); //resizes coords so that they will fit the canvas nicely
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = "#FFFFFF";
-    for (let i = 0; i < lines.length; i++){
-        ctx.beginPath();
-        ctx.moveTo(canCenX + finCoords[lines[i][0]][0], canCenY + finCoords[lines[i][0]][1]);
-        ctx.lineTo(canCenX + finCoords[lines[i][1]][0], canCenY + finCoords[lines[i][1]][1]);
-        ctx.stroke();
-    }
-}
-
-
-//Takes a point on the canvas, returns new set of unit vectors, scale can change how big the canvas is relative to the shape (needs implementing)
-function turnUnits(px, py, canDist, scale) {
-    const pVec = [scale*px, scale*py, canDist]; //vector of mouse pointer (scale will go here if needs be)
-    const pMag = vecMag(pVec); //Finds magnitude
-    const kuVec = vecScale((1/pMag), pVec); //Turns pVec into a unit vector. This is the same as the k unit vector due to the rotation point being (0,0,0)
-    const kjCross = vecCross(kuVec, [0,1,0]);
-    //const kjCross = vecCross([0,1,0], kuVec);
-    const kjcMag = vecMag(kjCross);
-    const iuVec = vecScale((-1/kjcMag), kjCross);
-    const juVec = vecCross(kuVec, iuVec);
-    //const juVec = vecCross(iuVec, kuVec);
-    return [iuVec, juVec, kuVec];
-}
-
-//Takes a coordinate and a set of unit vectors, rebuilds in the new basis
-function turnCoord(coord, units) {
-    const nx = vecScale(coord[0], units[0]);
-    const ny = vecScale(coord[1], units[1]);
-    const nz = vecScale(coord[2], units[2]);
-    let hold = vecAdd(vecAdd(nx, ny), nz);
-    if (coord.length == 4) {
-        hold.push(coord[3]);
-        return hold;
-    }
-    else {
-        return hold;
-    }
-}
-
-function turnPoints(coords, px, py, canDist) {
-    if (mouseFollow) {
-        const units = turnUnits(px, py, canDist, 1/100);
-        let hold = arrCopy(coords);
-        for (let i = 0; i < hold.length; i++){
-            hold[i] = turnCoord(hold[i], units)
+    for (let i = 0; i < world.length; i++) {
+        let coords = world[i].coords;
+        let lines = world[i].lines;
+        let finCoords = setPersp(camera, coords);
+        for (let i = 0; i < lines.length; i++){
+            ctx.beginPath();
+            ctx.moveTo(canCenX + finCoords[lines[i][0]][0], canCenY + finCoords[lines[i][0]][1]);
+            ctx.lineTo(canCenX + finCoords[lines[i][1]][0], canCenY + finCoords[lines[i][1]][1]);
+            ctx.stroke();
         }
-        render(hold)
     }
 }
+
+
 
 function testMouseFollow(canvas, coords, evt) {
     const bm = biggestMag(coords);
@@ -314,30 +240,16 @@ function testMouseFollow(canvas, coords, evt) {
     turnPoints(coords, mp[0] , mp[1] , canDist);
 }
 
-//returns object with mouse position
-function getMousePos(canvas, evt) {
-        var rect = canvas.getBoundingClientRect();
-        return [evt.clientX - rect.left, evt.clientY - rect.top];
-      }
 
 function mouseMove(evt) {
-    const bm = biggestMag(coords);
-    const canDist = 2*bm;
-    const mp = getMousePos(canvas, evt);
-    lastMousePos = mp;
-    const canCenX = document.getElementById("mainCanvas").width/2;
-    const canCenY = document.getElementById("mainCanvas").height/2;
-    turnPoints(coords, mp[0]-canCenX, mp[1]-canCenY, canDist);
+    if (mouseFollow){
+        upwardCamRot(camera, mouseRotSpeed*evt.movementY);
+        rightwardCamRot(camera, mouseRotSpeed*evt.movementX);
+        render(world);
+    }
 }
 
-function simulatedMouseMove(lastMousePos) {
-    const bm = biggestMag(coords);
-    const canDist = 2*bm;
-    const mp = lastMousePos
-    const canCenX = document.getElementById("mainCanvas").width/2;
-    const canCenY = document.getElementById("mainCanvas").height/2;
-    turnPoints(coords, mp[0]-canCenX, mp[1]-canCenY, canDist);
-}
+
 
 //keypress resolve
 function resolveKeyPress(e) {
@@ -365,7 +277,16 @@ function resolveKeyPress(e) {
     else if (e.keyCode == 75){
         upwardCamRot(camera, -1*rotSpeed);
     }
-    console.log(camera);
+    else if (e.keyCode == 77){
+        mouseFollow = mouseFollow ? false : true;
+    }
+    console.log(camera.xunit)
+    console.log(vecMag(camera.xunit))
+    console.log(camera.yunit)
+    console.log(vecMag(camera.yunit))
+    console.log(camera.zunit)
+    console.log(vecMag(camera.zunit))
+    render(world);
 }
 document.addEventListener('keydown', resolveKeyPress);
 
